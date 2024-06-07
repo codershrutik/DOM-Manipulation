7798f780995583b3a6b8cf656381c50e2ca5ce6c
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-07 18:28:13 +0000

    Added getElementsByClassName

--- Manipulating/app.js
+++ Manipulating/app.js
@@ -4,6 +4,8 @@ const allLinks = document.querySelectorAll('a');
 //     link.innerText = 'I AM A LINK!!!!'
 // }
 
+const squareImages = document.getElementsByClassName('square');
+
 
 for (let link of allLinks) {
     link.style.color = 'rgb(0, 108, 134)';
