f2eec041dac210618db7429702c83fcb76e4da41
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-07 19:04:49 +0000

    DOM styles

--- Manipulating/app.js
+++ Manipulating/app.js
@@ -1,14 +1,14 @@
-const allLinks = document.querySelectorAll('a');
+// const allLinks = document.querySelectorAll('a');
 
-for (let link of allLinks) {
-    link.innerText = 'I AM A LINK!!!!'
-}
+// for (let link of allLinks) {
+//     link.innerText = 'I AM A LINK!!!!'
+// }
 
 // const squareImages = document.getElementsByClassName('square');
 
 
-// for (let link of allLinks) {
-//     link.style.color = 'rgb(0, 108, 134)';
-//     link.style.textDecorationColor = 'magenta';
-//     link.style.textDecorationStyle = 'wavy'
-// }
+for (let link of allLinks) {
+    link.style.color = 'rgb(0, 108, 134)';
+    link.style.textDecorationColor = 'magenta';
+    link.style.textDecorationStyle = 'wavy'
+}
